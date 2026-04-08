b8484c8f081ff06fb012ac0e3cd2753d1fa2aaf1
WilliamReeds1 <reeds.w@wustl.edu>
2026-04-08 15:12:17 -0500

    Yea completed!

--- src/studio9/WordCount.java
+++ src/studio9/WordCount.java
@@ -8,45 +8,55 @@ import java.util.Map;
 import support.cse131.NotYetImplementedException;
 
 public class WordCount {
-	/**
-	 * Constructs and returns a map of the distinct words in the specified list with
-	 * each word associated with its accumulated count (that is: the number of
-	 * occurrences of the word in the list).
-	 * 
-	 * For example, if passed a List<String> containing:
-	 * 
-	 * [to, be, or, not, to, be]
-	 * 
-	 * the resulting Map<String, Integer> would contain
-	 * 
-	 * key="to", value=2;
-	 * key="be", value=2;
-	 * key="or", value=1;
-	 * key="not", value=1;
-	 * 
-	 * @param words
-	 * @return a map which contains all of the distinct words as keys, each
-	 *         associated with the number of occurrences of the word
-	 */
-	public static Map<String, Integer> countWords(List<String> words) {
 
-		// FIXME
-		throw new NotYetImplementedException();
+    /**
+     * Constructs and returns a map of the distinct words in the specified list
+     * with each word associated with its accumulated count (that is: the number
+     * of occurrences of the word in the list).
+     *
+     * For example, if passed a List<String> containing:
+     *
+     * [to, be, or, not, to, be]
+     *
+     * the resulting Map<String, Integer> would contain
+     *
+     * key="to", value=2; key="be", value=2; key="or", value=1; key="not",
+     * value=1;
+     *
+     * @param words
+     * @return a map which contains all of the distinct words as keys, each
+     * associated with the number of occurrences of the word
+     */
+    public static Map<String, Integer> countWords(List<String> words) {
+        Map<String, Integer> count = new HashMap<>();
+        for (int i = 0; i < words.size(); i++) {
+            String temp = words.get(i);
+            if (!count.containsKey(temp)) {
+                count.put(temp, 1);
+            } else {
+                count.put(temp, count.get(temp) + 1);
+            }
+        }
 
-	}
-	
-	public static void main(String[] args) {
-		
-		List<String> wordList = new LinkedList<>();
-		wordList.add("to");
-		wordList.add("be");
-		wordList.add("or");
-		wordList.add("not");
-		wordList.add("to");
-		wordList.add("be");
-		Map<String, Integer> words = countWords(wordList);
-		
-		//TODO: Write code that will iterate over the words map
-		//to verify its contents
-	}
+        return count;
+    }
+
+    public static void main(String[] args) {
+
+        List<String> wordList = new LinkedList<>();
+        wordList.add("to");
+        wordList.add("be");
+        wordList.add("or");
+        wordList.add("not");
+        wordList.add("to");
+        wordList.add("be");
+        Map<String, Integer> words = countWords(wordList);
+
+        //TODO: Write code that will iterate over the words map
+        //to verify its contents
+        for (String s : words.keySet()) {
+            System.out.println(s);
+            System.out.println(words.get(s));
+        }
+    }
 }
